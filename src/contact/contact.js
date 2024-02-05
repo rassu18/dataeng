11c80da79de094043a6dc73b80789704592ed0fc
rassu18 <105088829+rassu18@users.noreply.github.com>
2024-02-05 19:01:16 +0530

    Update contact.js

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -1,6 +1,8 @@
 import React from "react";
 
-function Contact() {
-  return <div>Hello Contact</div>;
-}
+const Contact = ()=> {
+  return( 
+  <div>Hello Contact</div>
+  );
+};
 export default Contact;

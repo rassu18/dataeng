f69e73f9c69a7e7546c4902588486e2e10422648
rassu18 <105088829+rassu18@users.noreply.github.com>
2024-03-03 23:34:12 +0530

    Update nav.js

--- src/pages/nav.js
+++ src/pages/nav.js
@@ -1,32 +1,48 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./nav.css";
-export const Navbar =() =>{
-    const [menuOpen, setMenuOpen]= useState(false);
-return(
+export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
     <nav>
-         <Link to='/' className="title">DigiSite</Link>
-         <div className="menu" onClick={()=>{
-            setMenuOpen(!menuOpen);
-         }}>
+      <Link to="/" className="title">
+        DigiSite
+      </Link>
+      <div
+        className="menu"
+        onClick={() => {
+          setMenuOpen(!menuOpen);
+        }}
+      >
         <span></span>
         <span></span>
         <span></span>
-         </div>
-       <ul className={menuOpen? "open": ""}>
+      </div>
+      <ul className={menuOpen ? "open" : ""}>
         <li className="nav-item dropdown">
-            <NavLink to='/' >Digital Engineering Services</NavLink>
-            <div className="dropdown-menu">
-        <NavLink to='/dataInside' className='dropdown-item'>Data Insights</NavLink>
-        </div>
+          <NavLink
+            className="dropdown-toggle"
+            id="navbarDropdownMenuLink"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+            to="/"
+          >
+            Digital Engineering Services
+          </NavLink>
+          <div className="dropdown-menu">
+            <NavLink to="/dataInside" className="dropdown-item">
+              Data Insights
+            </NavLink>
+          </div>
         </li>
         <li>
-            <NavLink to='/about'>About</NavLink>
+          <NavLink to="/about">About</NavLink>
         </li>
         <li>
-            <NavLink to='/contact'>Contact</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
         </li>
-        </ul> 
+      </ul>
     </nav>
-)
-}
+  );
+};

a10eef5397ba24a9eeb8a55f3df0cdff37025fdb
rassu18 <105088829+rassu18@users.noreply.github.com>
2024-03-14 11:27:40 +0530

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -28,12 +28,12 @@ function Home() {
         <div className="col-md-12 marginclient">
           <h2 className="mx-5">Programmatic Innovation Driven Business Transformation</h2>
           <div className="container mt-4">
-           <div className="col-md-4">
+           <div >
            Using our own Programmatic Innovation framework, DigiSite digital engineering services are intended to assist you in moving from "where you are today" to "where you want to go." We bind together the whole software development process, which enables us to give our cooperating organisations a predictable result.
 
 With honesty, reliability, and compassion serving as our guiding principles, we deliver predictable results to the final customer. which serve as the foundation for the digitally resilient businesses we assist our clients in creating.
            </div>
-           <div className="col-md-2 mt-4">
+           <div className="readmore">
                 <button className="btn btn-warning dataInsidebtn">Read More</button>
             </div>
           </div>
